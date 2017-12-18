fe36a460dd5427b39cd3d89b219a6170aa5cd385
sfrazee <sfrazee@RIDGEBACK.communitycompounding.com>
2017-12-18 09:11:20 -0800

    Removed unused imports

--- vetsource-autoprovision/src/com/vetsource/sfrazee/autoprovision/configmanager/ConfigManager.java
+++ vetsource-autoprovision/src/com/vetsource/sfrazee/autoprovision/configmanager/ConfigManager.java
@@ -1,10 +1,6 @@
 package com.vetsource.sfrazee.autoprovision.configmanager;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
